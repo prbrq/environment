5d3db48de40d23bddf9d928101f4d010a04b0c61
Arkady <prbrq@ya.ru>
2024-08-13 22:47:02 +0300

    revert: :bulb: Обновлены TODO

--- Scripts/install_yay.sh
+++ Scripts/install_yay.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# TODO: Устанавливать yay только если он не установлен
+
 git clone https://aur.archlinux.org/yay.git ~/Projects/yay
 cd ~/Projects/yay
 makepkg -si
--- Scripts/setup_keepass_plugins.sh
+++ Scripts/setup_keepass_plugins.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# TODO: Check if plugin already downloaded
+# TODO: Загружать плагины только если они ещё не были загружены
 sudo wget https://github.com/KoenZomers/KeePassOneDriveSync/releases/download/2.1.2.2/KeeOneDriveSync.plgx -O /usr/share/keepass/Plugins/KeeOneDriveSync.plgx
