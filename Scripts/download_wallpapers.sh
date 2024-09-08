f57f73a0e1f49714e02ff7f9c03a35bdee328ad8
Arkady <prbrq@ya.ru>
2024-09-08 10:58:22 +0300

    feat(Wallpapers): :sparkles: Добавлена загрузка обоев для рабочего стола

---
+++ Scripts/download_wallpapers.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget https://w.wallhaven.cc/full/zm/wallhaven-zm11rg.jpg -O ~/Pictures/wallhaven-zm11rg.jpg
+wget https://w.wallhaven.cc/full/2k/wallhaven-2k5v6g.jpg -O ~/Pictures/wallhaven-2k5v6g.jpg
+wget https://w.wallhaven.cc/full/jx/wallhaven-jxzy65.jpg -O ~/Pictures/wallhaven-jxzy65.jpg
+wget https://w.wallhaven.cc/full/ox/wallhaven-oxmrkp.jpg -O ~/Pictures/wallhaven-oxmrkp.jpg
